f7cc209ba354e99be619087b07dbdf98ca2dc96e
Joey Emerson <j.emerson.studio@gmail.com>
2019-11-13 13:24:48 -0600

    formatted triangle file

--- 2kyu_insane_coloured_triangles.py
+++ 2kyu_insane_coloured_triangles.py
@@ -1,5 +1,6 @@
 import random
 
+
 # Main function steps through rows and calls outside functions
 # to update next row contents
 def triangle(row):
@@ -7,19 +8,20 @@ def triangle(row):
         row = next_row(row)
     return row
 
+
 # Returns contents of the next row in series
 def next_row(row):
-    result = ''
+    result = ""
 
     # Keys are the values returned when calculating difference
     # in ordinal numbers for character values
-    rgb_lookup = {5: 'R', 16: 'G', 11: 'B'}
-    previous = ''
+    rgb_lookup = {5: "R", 16: "G", 11: "B"}
+    previous = ""
 
     first = True
     for item in row:
         # Skip first item in each row
-        if first == True:
+        if first is True:
             first = False
             previous = item
             continue
@@ -30,6 +32,7 @@ def next_row(row):
         previous = item
     return result
 
+
 # Was hoping to find some correlation between character counts
 # and the final row, but does not seem to matter
 def row_stats(row):
@@ -38,15 +41,19 @@ def row_stats(row):
         counts[x] = counts.get(x, 0) + 1
     return counts
 
+
 # Everything below is currently set to export to csv. Analysis unsuccessful...
 print("row_length,final_row,first_letter,last_letter,count_r,count_g,count_b")
 counter = 1
 
 while counter <= 1000:
-    string = ''
-    ubound = random.randint(25,100)
-    for x in range(1, ubound):
-        string += random.choice('RGB')
+    string = ""
+    ubound = random.randint(25, 100)
+    for _ in range(1, ubound):
+        string += random.choice("RGB")
     counts = row_stats(string)
-    print(f"{ubound},{triangle(string)},{string[0]},{string[len(string) - 1]},{counts['R']},{counts['G']},{counts['B']}")
+    print(
+        f"{ubound},{triangle(string)},{string[0]},{string[len(string) - 1]},"
+        + f"{counts['R']},{counts['G']},{counts['B']}"
+    )
     counter += 1
