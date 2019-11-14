d8a9a163cf521bb6907add0b6994384b6a959c89
Joey Emerson <j.emerson.studio@gmail.com>
2019-11-13 21:45:44 -0600

    formatted file

--- 4kyu_sudoku_solution_validator.py
+++ 4kyu_sudoku_solution_validator.py
@@ -1,54 +1,81 @@
 # Sudoku Background
 
-# Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
+# Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all
+# cells of the grid with digits from 1 to 9, so that each column, each row,
+# and each of the nine 3x3 sub-grids (also known as blocks) contain all of the
+# digits from 1 to 9.
 # (More info at: http://en.wikipedia.org/wiki/Sudoku)
 # Sudoku Solution Validator
 
-# Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+# Write a function validSolution/ValidateSolution/valid_solution() that
+# accepts a 2D array representing a Sudoku board, and returns true if it is a
+# valid solution, or false otherwise. The cells of the sudoku board may also
+# contain 0's, which will represent empty cells. Boards containing one or more
+# zeroes are considered to be invalid solutions.
 
-# The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+# The board is always 9 cells by 9 cells, and every cell only contains
+# integers from 0 to 9.
+
 
 def is_valid_solution(board):
-    
     check = list(range(1, 10))
 
     # Check rows
     for row in board:
-        if sorted(row) != check: return False
+        if sorted(row) != check:
+            return False
 
     # Check columns
     for row in zip(*board):
-        if sorted(row) != check: return False
+        if sorted(row) != check:
+            return False
 
     # Check each 3x3 grid
     for i in range(0, 9, 3):
         for j in range(0, 9, 3):
-            nums = board[i][j:j+3] + board[i+1][j:j+3] + board[i+2][j:j+3]
-            if sorted(nums) != check: return False
+            nums = (
+                board[i][j:j + 3]
+                + board[i + 1][j:j + 3]
+                + board[i + 2][j:j + 3]
+            )
+            if sorted(nums) != check:
+                return False
             del nums
-            
-    # If all checks pass, return True       
+
+    # If all checks pass, return True
     return True
 
 
 # This test should equal True
-print(is_valid_solution([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-                         [6, 7, 2, 1, 9, 5, 3, 4, 8],
-                         [1, 9, 8, 3, 4, 2, 5, 6, 7],
-                         [8, 5, 9, 7, 6, 1, 4, 2, 3],
-                         [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                         [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                         [9, 6, 1, 5, 3, 7, 2, 8, 4],
-                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                         [3, 4, 5, 2, 8, 6, 1, 7, 9]]))
+print(
+    is_valid_solution(
+        [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9],
+        ]
+    )
+)
 
 # This test should equal False
-print(is_valid_solution([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-                         [6, 7, 2, 1, 9, 0, 3, 4, 9],
-                         [1, 0, 0, 3, 4, 2, 5, 6, 0],
-                         [8, 5, 9, 7, 6, 1, 0, 2, 0],
-                         [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                         [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                         [9, 0, 1, 5, 3, 7, 2, 1, 4],
-                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                         [3, 0, 0, 4, 8, 1, 1, 7, 9]]))
+print(
+    is_valid_solution(
+        [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 0, 3, 4, 9],
+            [1, 0, 0, 3, 4, 2, 5, 6, 0],
+            [8, 5, 9, 7, 6, 1, 0, 2, 0],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 0, 1, 5, 3, 7, 2, 1, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 0, 0, 4, 8, 1, 1, 7, 9],
+        ]
+    )
+)
